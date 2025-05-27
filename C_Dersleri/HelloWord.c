ade5e5ea502a7c7eb78bf5991a78f4ef1ead06a3
Özgür Güngör <168583984+ozgurgungor1@users.noreply.github.com>
2025-05-28 01:20:01 +0300

    Add files via upload

---
+++ C_Dersleri/HelloWord.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello Word\n");
+    printf("Özgür Güngör\n");
+
+
+
+
+
+
+//yorum satırıı :)
+
+
+/*
+
+\n yeni satır
+\t  geniş boşluk
+
+
+
+*/
+
+
+printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+
+
+
+
+
+
+
+int x; // tanımla
+x = 123; //değer verme
+int y = 321; //tanımla + deger verme      en iyisi bu şekilde kullanım
+
+int level = 85;  //tam sayılar
+float xp = 12.06; //ondalıklı sayılar
+char sever = 'A'; //tek karakter
+char nick[] = "XaoF"; //karakter dizisi
+
+printf("Merhaba %s\n", nick);
+printf("Şuan %d levele sahipsin\n", level);
+printf("Xp durumun %%%f\n",xp);
+printf("%d Sunucusunda oynuyorsun", sever);
+
+
+
+
+
+    return 0;
+}
